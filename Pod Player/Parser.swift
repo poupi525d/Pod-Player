0c48722d4382a872a4c17a57e723c505baecd79c
RALAIZEFA Michael <m.ralaizefa@laposte.net>
2018-05-24 19:49:36 +0200

    Accès coreData

--- Pod Player/Parser.swift
+++ Pod Player/Parser.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+class Parser {
+    func getPodcatMetaData(data:Data) -> (title:String?, imageURL:String?) {
+        let xml = SWXMLHash.parse(data)
+        
+        print(xml["rss"]["channel"]["itunes:image"].element?.attribute(by: "href")?.text)
+        return (xml["rss"]["channel"]["title"].element?.text, xml["rss"]["channel"]["itunes:image"].element?.attribute(by: "href")?.text)
+    }
+}
--- Pod Player/PodcastViewController.swift
+++ Pod Player/PodcastViewController.swift
@@ -10,9 +10,28 @@ import Cocoa
 
 class PodcastViewController: NSViewController {
 
+    @IBOutlet weak var podcastURLTextField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        podcastURLTextField.stringValue = "http://www.espn.com/espnradio/feeds/rss/podcast.xml?id=2406595"
     }
     
+    @IBAction func AjouterPodcastClicked(_ sender: Any) {
+        
+        URLSession.shared.dataTask(with: URL(string: podcastURLTextField.stringValue)!) { (data:Data?, reponse:URLResponse?, error:Error?) in
+            if error != nil {
+                print(error!)
+            }else{
+                if data != nil {
+                    
+                let parser = Parser()
+                let info = parser.getPodcatMetaData(data: data!)
+                    print(info)
+                }
+            }
+        }.resume()
+        podcastURLTextField.stringValue = ""
+    }
 }
