908b29a72dd666a604db30c41899db135d8198a7
RALAIZEFA Michael <m.ralaizefa@laposte.net>
2018-05-24 19:48:50 +0200

    Accès coreData

---
+++ Pod Player/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  Pod Player
+//
+//  Created by Michaël on 24/05/2018.
+//  Copyright © 2018 Michaël. All rights reserved.
+//
+
+import Foundation
---
+++ Pod Player/PodcastViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PodcastViewController.swift
+//  Pod Player
+//
+//  Created by Michaël on 24/05/2018.
+//  Copyright © 2018 Michaël. All rights reserved.
+//
+
+import Cocoa
+
+class PodcastViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
